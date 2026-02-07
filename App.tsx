4f5972c8de534591c7814fb0eb5107bb55813ae2
maharsh.s <maharsh.soni@pixelque.com>
2026-02-08 00:52:12 +0530

    Refactor App component to include ApolloProvider and GestureHandlerRootView; update GraphQL client setup and add rxjs dependency

--- App.tsx
+++ App.tsx
@@ -1,9 +1,25 @@
-
 import React from 'react'
 import Navigation from './src/navigation/Navigation'
+import { ApolloProvider } from '@apollo/client/react'
+import { client } from './src/graphQL/client'
+import 'react-native-gesture-handler'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
+import { StyleSheet } from 'react-native'
 
-export default function App() {
+const App = () => {
   return (
-    <Navigation />
+    <GestureHandlerRootView style={styles.container}>
+      <ApolloProvider client={client}>
+        <Navigation />
+      </ApolloProvider>
+    </GestureHandlerRootView>
   )
 }
+
+export default App
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+})
--- src/graphQL/client.ts
+++ src/graphQL/client.ts
@@ -1,26 +1,28 @@
-import {ApolloClient, InMemoryCache, createHttpLink} from '@apollo/client';
-import {setContext} from '@apollo/client/link/context';
-import {mmkvStorage} from '../state/storage';
-import {Platform} from 'react-native';
+import { ApolloClient, InMemoryCache, HttpLink, ApolloLink } from '@apollo/client'
+import { Platform } from 'react-native'
+import { mmkvStorage } from '../state/storage'
 
-const httpLink = createHttpLink({
+const httpLink = new HttpLink({
   uri:
     Platform.OS === 'ios'
       ? 'http://localhost:3000/api/graphql'
       : 'http://10.0.2.2:3000/api/graphql',
-});
+})
 
-const authLink = setContext((_, {headers}) => {
-  const token = mmkvStorage.getItem('token');
-  return {
+const authLink = new ApolloLink((operation, forward) => {
+  const token = mmkvStorage.getItem('token')
+
+  operation.setContext(({ headers = {} }) => ({
     headers: {
       ...headers,
       authorization: token ? `Bearer ${token}` : '',
     },
-  };
-});
+  }))
+
+  return forward(operation)
+})
 
 export const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: ApolloLink.from([authLink, httpLink]),
   cache: new InMemoryCache(),
-});
+})
