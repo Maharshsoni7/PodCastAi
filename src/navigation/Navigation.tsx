37c206538aaaaf73d0d0f3499a37eb70b8d4d3e4
maharsh.s <maharsh.soni@pixelque.com>
2026-02-08 00:23:05 +0530

    Add UserBottomTab with Home, Search, and Favourite screens; create HomeScreen, SearchScreen, and FavouriteScreen components

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -6,6 +6,7 @@ import { navigationRef } from '../utils/NavigationUtils'
 import SplashScreen from '../features/auth/SplashScreen'
 import LoginScreen from '../features/auth/LoginScreen'
 import RegisterScreen from '../features/auth/RegisterScreen'
+import UserBottomTab from '../features/tabs/UserBottomTab'
 const Stack = createNativeStackNavigator()
 const Navigation = () => {
     return (
@@ -14,6 +15,7 @@ const Navigation = () => {
                 <Stack.Screen name='SplashScreen' component={SplashScreen} />
                 <Stack.Screen name='LoginScreen' component={LoginScreen} />
                 <Stack.Screen name='RegisterScreen' component={RegisterScreen} />
+                <Stack.Screen name='UserBottomTab' component={UserBottomTab} />
             </Stack.Navigator>
         </NavigationContainer>
     )
